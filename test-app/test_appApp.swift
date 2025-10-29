3e14eafaf7537df799c440160f61fb06d6d7c07f
Nishan Kumar <nishan.kumar@7edge.com>
2025-10-29 11:48:34 +0530

    Initial Commit

---
+++ test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Nishan Kumar on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
